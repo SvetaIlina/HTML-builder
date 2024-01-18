aa1419234f10db10f58561072768002d580048df
Svetlana <svetailina1987@gmail.com>
2024-01-18 11:53:43 +0400

    feat: add recursive copying of folders in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,23 +1,46 @@
 const fs = require('fs/promises');
 const path = require('path');
+const origin = path.resolve(__dirname, 'files');
+const copy = path.resolve(__dirname, 'files-copy');
 
-async function copyDir() {
+async function copyDir(originFolder, copyFolder) {
   try {
-    await fs.rm(path.resolve(__dirname, 'files-copy'), {
+    await fs.rm(copyFolder, {
       force: true,
       recursive: true,
     });
-    await fs.mkdir(path.resolve(__dirname, 'files-copy'), { recursive: true });
-    const files = await fs.readdir(path.resolve(__dirname, 'files'));
+    await fs.mkdir(copyFolder, { recursive: true });
+    const files = await fs.readdir(originFolder, { withFileTypes: true });
     for (let i = 0; i < files.length; i++) {
-      await fs.copyFile(
-        path.resolve(__dirname, 'files', files[i]),
-        path.resolve(__dirname, 'files-copy', files[i]),
-      );
+      const currentFile = path.join(originFolder, files[i].name);
+      const copyFile = path.join(copyFolder, files[i].name);
+      if (!files[i].isDirectory()) {
+        await fs.copyFile(currentFile, copyFile);
+      } else {
+        copyDir(currentFile, copyFile);
+      }
     }
   } catch (err) {
     console.log(err.message);
   }
 }
+// async function copyDir() {
+//   try {
+//     await fs.rm(path.resolve(__dirname, 'files-copy'), {
+//       force: true,
+//       recursive: true,
+//     });
+//     await fs.mkdir(path.resolve(__dirname, 'files-copy'), { recursive: true });
+//     const files = await fs.readdir(path.resolve(__dirname, 'files'));
+//     for (let i = 0; i < files.length; i++) {
+//       await fs.copyFile(
+//         path.resolve(__dirname, 'files', files[i]),
+//         path.resolve(__dirname, 'files-copy', files[i]),
+//       );
+//     }
+//   } catch (err) {
+//     console.log(err.message);
+//   }
+// }
 
-copyDir();
+copyDir(origin, copy);
